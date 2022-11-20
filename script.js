d093e4dbfba058bff011d0222fa61f11695430eb
umprej <jan.umprecht@gmail.com>
2022-11-20 21:23:54 +0000

    Add one-round play functionality

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const VALUES = [1, 1, 2, 7, 8, 9, 10, 11];
+
 function reset() {
 
+}
+
+function dealCard() {
+    return VALUES[Math.floor(Math.random() * VALUES.length)];
+}
+
+function botPlay() {
+    let sum = dealCard();
+    while (sum < 17) {
+        sum += dealCard();
+    }
+    return sum;
 }
 
 class Game {
@@ -9,19 +23,52 @@ class Game {
         this.botScore = 0;
         this.currBet = 0;
     }
+
+    hit() {
+        this.playerScore += dealCard();
+        if(this.playerScore > 21) {
+            this.stay();
+        }
+        document.querySelector('#player-sum').textContent = this.playerScore;
+
+    }
+
+    stay() {
+        this.botScore = botPlay();
+        alert(`Your score is ${this.playerScore}, bot scored ${this.botScore}`);
+    }
 }
 
 function game() {
     let game = new Game();
-    let betSubmitBtn = document.querySelector("#bet-button");
 
-    betSubmitBtn.addEventListener('click', (event) => {
+    
+    //initial bet
+    let betButton = document.querySelector("#bet-button");
+    betButton.addEventListener('click', (event) => {
         let betField = document.querySelector("#bet-field");
         game.playerBalance = betField.value;
         document.querySelector('#curr-balance').textContent = game.playerBalance;
 
-        document.querySelector("#bet-field").setAttribute("disabled", "");
-    })
+        document.querySelectorAll('.bet').forEach(elem =>
+            elem.style.display = "none");
+
+        game.hit();
+    });
+
+    //hit
+    let hitButton = document.querySelector("#hit-button");
+    hitButton.addEventListener('click', (event) => {
+        game.hit();
+    });
+
+    //stay
+    let stayButton = document.querySelector("#stay-button");
+    stayButton.addEventListener('click', (event) => {
+        game.stay();
+    });
+
+
 }
 
 game()
